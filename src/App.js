a15e1a243d5cce248a905ea5618c1e4f10d28f13
Amit Kumar <amit_kumar@rakudo.io>
2020-12-03 10:37:24 +0900

    sort and order

--- src/App.js
+++ src/App.js
@@ -1,13 +1,70 @@
+import React from "react";
+import Filter from "./components/Filter";
+import Products from "./components/Products";
+import data from "./data.json";
 
-
-function App() {
-  return (
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  sortProducts = (event) => {
+    const sort = event.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+      .slice()
+      .sort((a,b) =>
+          sort === "lowest"
+           ? a.price > b.price 
+            ? 1
+            : -1
+          : sort === "highest" 
+          ? a.price < b.price 
+            ? 1
+            : -1
+          : a._id < b._id 
+            ? 1
+            : -1
+        )   //this is filtered products
+    }))
+  }
+  filterProducts = (event) =>{
+    if(event.target.value === ""){
+      this.setState({size:event.target.value,products:data.products})
+    } else {
+      this.setState(
+        {
+          size: event.target.value,
+          products: data.products.filter(product => product.availableSizes.indexOf(event.target.value)>=0)
+        }
+      )
+    }
+   
+  }
+  render(){
+    return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping Cart</a>
       </header>
       <main>
-        Product List
+        <div className="content">
+          <div className="main">
+            <Filter count={this.state.products.length}
+            size={this.state.size}
+            sort={this.state.sort}
+            filterProducts={this.filterProducts}
+            sortProducts={this.sortProducts}
+            ></Filter>
+            <Products products={this.state.products}></Products>
+          </div>
+          <div className="sidebar">Cart Items</div>
+        </div>
       </main>
       <footer>
         All rights is reserved.
@@ -15,6 +72,7 @@ function App() {
     </div>
     
   );
+    }
 }
 
 export default App;
